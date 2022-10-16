b1c5952fed07c7410ddd995b12d17e6b032d468e
Joel Wong <45819490+joelwongjy@users.noreply.github.com>
2022-10-15 17:45:07 -0700

    Initial Commit

---
+++ HowYa/HowYaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowYaApp.swift
+//  HowYa
+//
+//  Created by Joel Wong on 15/10/22.
+//
+
+import SwiftUI
+
+@main
+struct HowYaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
